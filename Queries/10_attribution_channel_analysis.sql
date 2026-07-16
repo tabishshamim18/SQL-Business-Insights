5fa5d553d99f4cfd0ba212f36343a85b8d9bb74b
Tabish Shamim <tabishshamim9@gmail.com>
2026-07-16 12:18:54 +0530

    Update 10_attribution_channel_analysis.sql

--- Queries/10_attribution_channel_analysis.sql
+++ Queries/10_attribution_channel_analysis.sql
@@ -14,22 +14,22 @@ WITH touches
      first_touch_tbl
      AS ( SELECT   'First_touch'                                 AS attribution_model,
                    Coalesce(channel, 'direct')                   AS channel,
-                   Sum(total) FILTER(WHERE first_touch = 1)      AS first_touch_revenue,
-                   Count(order_id) FILTER(WHERE first_touch = 1) AS first_touch_orders
+                   Sum(total) FILTER(WHERE first_touch = 1)      AS revenue,
+                   Count(order_id) FILTER(WHERE first_touch = 1) AS orders
           FROM     touches
           GROUP BY channel ), 
      last_touch_tbl
      AS ( SELECT   'Last_touch'                                 AS attribution_model,
                    Coalesce(channel, 'direct')                  AS channel,
-                   Sum(total) FILTER(WHERE last_touch = 1)      AS last_touch_revenue,
-                   Count(order_id) FILTER(WHERE last_touch = 1) AS last_touch_orders
+                   Sum(total) FILTER(WHERE last_touch = 1)      AS revenue,
+                   Count(order_id) FILTER(WHERE last_touch = 1) AS orders
           FROM     touches
           GROUP BY channel ) 
   SELECT *,
-         100.0 * first_touch_revenue / Sum(first_touch_revenue) OVER() AS share_of_revenue
+         100.0 * revenue / Sum(revenue) OVER() AS share_of_revenue
   FROM   first_touch_tbl
   UNION ALL
   SELECT *,
-         100.0 * last_touch_revenue / Sum(last_touch_revenue) OVER() AS share_of_revenue
+         100.0 * revenue / Sum(revenue) OVER() AS share_of_revenue
   FROM   last_touch_tbl
-  ORDER BY first_touch_revenue DESC
+  ORDER BY revenue DESC
