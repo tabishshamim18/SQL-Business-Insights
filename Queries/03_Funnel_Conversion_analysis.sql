fa2fa98721bf2e681d7268a3e6d23073175eb320
Tabish Shamim <tabishshamim9@gmail.com>
2026-07-16 11:55:00 +0530

    Update 03_Funnel_Conversion_analysis.sql

--- Queries/03_Funnel_Conversion_analysis.sql
+++ Queries/03_Funnel_Conversion_analysis.sql
@@ -10,9 +10,10 @@ WITH sessions_base
                    ON e.session_id = c.session_id
           WHERE    Date(e.occurred_at) >= '2026-04-19'
           GROUP BY Coalesce(c.channel,'direct')) 
-  SELECT *,
-         100.0 * add_to_cart_sessions / Nullif(product_view_sessions,0)   AS view_to_cart_rate,
-         100.0 * begin_checkout_sessions / Nullif(add_to_cart_sessions,0) AS cart_to_checkout_rate,
-         100.0 * purchase_sessions / Nullif(begin_checkout_sessions,0)    AS checkout_to_purchase_rate,
-         100.0 * purchase_sessions / Nullif(sessions,0)                   AS session_to_purchase_rate
-  FROM   sessions_base
+  SELECT 	*,
+         	100.0 * add_to_cart_sessions / Nullif(product_view_sessions,0)   AS view_to_cart_rate,
+        	100.0 * begin_checkout_sessions / Nullif(add_to_cart_sessions,0) AS cart_to_checkout_rate,
+         	100.0 * purchase_sessions / Nullif(begin_checkout_sessions,0)    AS checkout_to_purchase_rate,
+         	100.0 * purchase_sessions / Nullif(sessions,0)                   AS session_to_purchase_rate
+  FROM   	sessions_base
+  ORDER BY  sessions DESC
